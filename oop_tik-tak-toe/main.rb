2c0bb7fc2382770fcb390d6b0e57ef818dca94a0
janak444 <tripathi.janak@gmail.com>
2024-02-14 11:25:26 +0545

    remove comment from the file

--- oop_tik-tak-toe/main.rb
+++ oop_tik-tak-toe/main.rb
@@ -8,13 +8,6 @@ require_relative 'moves'
 # main class runs the program, it consist of all the dependencies and call all the classes to run.
 class Main
   def initialize
-    # puts 'Enter the size of the board'
-    # loop do
-    #   @size = gets.chomp.to_i
-    #   puts 'please enter the size between 3 and 10' if @size < 3 || @size > 10
-
-    #   break if @size > 2 && @size < 11
-    # end
     @size = 3
     @board = Board.new(@size)
     @moves = Moves.new(@board)
--- oop_tik-tak-toe/moves.rb
+++ oop_tik-tak-toe/moves.rb
@@ -10,50 +10,6 @@ class Moves
   def player_turn(player)
     puts "Player #{player} (#{player}), enter your move from 1 to 9 :"
     print '> '
-    # move = gets.chomp.split.map(&:to_i)
-    # static
-    # case move[0]
-    # when 1
-    #   move[0] = 0
-    #   move[1] = 0
-    # when 2
-    #   move[0] = 0
-    #   move[1] = 1
-    # when 3
-    #   move[0] = 0
-    #   move[1] = 2
-    # when 4
-    #   move[0] = 1
-    #   move[1] = 0
-    # when 5
-    #   move[0] = 1
-    #   move[1] = 1
-    # when 6
-    #   move[0] = 1
-    #   move[1] = 2
-    # when 7
-    #   move[0] = 2
-    #   move[1] = 0
-    # when 8
-    #   move[0] = 2
-    #   move[1] = 1
-    # when 9
-    #   move[0] = 2
-    #   move[1] = 2
-    # end
-
-    # use hash map to shorten the code
-    # move_map = {
-    #   1 => [0, 0],
-    #   2 => [0, 1],
-    #   3 => [0, 2],
-    #   4 => [1, 0],
-    #   5 => [1, 1],
-    #   6 => [1, 2],
-    #   7 => [2, 0],
-    #   8 => [2, 1],
-    #   9 => [2, 2]
-    # }
 
     # dynamic hash function
     move_map = Hash.new { |has, key| has[key] = [] }
